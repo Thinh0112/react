fe588f79b80425633ffda14accd7bafd5af96103
unknown <phanbathinh01121996@gmail.com>
2022-03-01 21:37:51 +0700

    react_buoi2_state_renderWithMap

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import HeaderClass from "./components/HeaderClass";
 import HomeComponent from "./components/BaiTapLayout/HomeComponent";
 import Databinding from "./DataBinding/Databinding";
 import HandleEvent from "./HandleEvent/HandleEvent";
+import Rendering_Conditions from "./Rendering_Conditions/Rendering_Conditions";
+import Ex1StateDemo from "./StateDemo/Ex1StateDemo";
+import RenderWithMap from "./RenderWithMap/RenderWithMap";
+import QuanLyDanhSachPhim from "./RenderWithMap/QuanLyDanhSachPhim";
 
 function App() {
   return (
     <div className="App">
-      <HandleEvent />
+      <QuanLyDanhSachPhim />
+      {/* <RenderWithMap /> */}
+      {/* <Ex1StateDemo /> */}
+      {/* <Rendering_Conditions /> */}
+      {/* <HandleEvent /> */}
       {/* <Databinding /> */}
       {/* <HomeComponent /> */}
       {/* Hello Cybersoft
